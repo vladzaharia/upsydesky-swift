e1aa0dccc25622a0bd4d4bb6a22db8e17470fda9
Vlad Zaharia <hey@vlad.gg>
2023-01-04 17:13:30 -0800

    Initial Commit

---
+++ UpsyDeskyMenu/UpsyDeskyMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpsyDeskyMenuApp.swift
+//  UpsyDeskyMenu
+//
+//  Created by Vlad Zaharia on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UpsyDeskyMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
